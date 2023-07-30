ffb9bf1e0ea50bfbf52f248113882ca5b79c006c
psinghcan <psinghcan@hotmail.com>
2023-07-30 11:17:32 -0400

    adding model/interface

--- ui/src/app/services/category.service.ts
+++ ui/src/app/services/category.service.ts
@@ -1,7 +1,16 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams, HttpResponse} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import {Category, NewCategory} from "../model/Category";
+import {createRequestOption} from "../utils/request-util";
+import {map, Observable} from "rxjs";
 
+export type EntityArrayResponseType = HttpResponse<Category>
+type RestOf<T extends Category | NewCategory> = Omit<T, 'dateAdded' | 'dateModified'> & {
+  dateAdded?: string | null;
+  dateModified?: string | null;
+};
+export type RestCategory = RestOf<Category>;
 
 @Injectable({
   providedIn: 'root'
@@ -11,23 +20,55 @@ export class CategoryService {
 
   constructor(private httpClient: HttpClient) { }
 
-  add(data:any){
-    return this.httpClient.post(this.url +"/category/add" , data,{
-      headers: new HttpHeaders().set('Content-Type' , "application/json")
-    })
+  // add(data:any){
+  //   return this.httpClient.post(this.url +"/category/add" , data,{
+  //     headers: new HttpHeaders().set('Content-Type' , "application/json")
+  //   })
+  // }
+  //
+  // update(data:any){
+  //   return this.httpClient.post(this.url +"/category/update" , data,{
+  //     headers: new HttpHeaders().set('Content-Type' , "application/json")
+  //   })
+  // }
+  //
+  // getCategories(){
+  //   return this.httpClient.get(this.url + "/category");
+  // }
+  //
+  // getFilteredCategories(){
+  //   return this.httpClient.get(this.url + "/category/get?filterValue=true");
+  // }
+
+  // getCategories(req?: any): Observable<EntityArrayResponseType> {
+  //   const options = createRequestOption(req);
+  //   return this.httpClient
+  //     .get<RestCategory[]>(this.url, { params: options, observe: 'response' })
+  //     .pipe(map(res => this.convertResponseArrayFromServer(res)));
+  //
+  // }
+  //
+  // protected convertResponseArrayFromServer(res: HttpResponse<RestCategory[]>): HttpResponse<Category[]> {
+  //   return res.clone({
+  //     body: res.body ? res.body.map(item => this.convertDateFromServer(item)) : null,
+  //   });
+  // }
+
+  public getCategories(): Observable<Category[]> {
+    return this.httpClient.get<Category[]>(`${this.url}/category/all`);
   }
 
-  update(data:any){
-    return this.httpClient.post(this.url +"/category/update" , data,{
-      headers: new HttpHeaders().set('Content-Type' , "application/json")
-    })
+  public getCategory(categoryId: number): Observable<Category> {
+    return this.httpClient.get<Category>(`${this.url}/category/${categoryId}`);
   }
 
-  getCategories(){
-    return this.httpClient.get(this.url + "/category");
+  public updateCategory(category: Category): Observable<Category> {
+    return this.httpClient.put<Category>(`${this.url}/category`, category);
   }
 
-  getFilteredCategories(){
-    return this.httpClient.get(this.url + "/category/get?filterValue=true");
+  public addCategory(category: Category): Observable<Category> {
+    return this.httpClient.post<Category>(`${this.url}/category`, category);
   }
+
+
 }
